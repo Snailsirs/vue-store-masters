466285b995a7c878ad97458516c9e6a260896e69
Snailsirs <1532724669@qq.com>
2020-03-23 13:50:40 +0800

    '二'

--- src/store/index.js
+++ src/store/index.js
@@ -8,15 +8,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import user from './modules/user'
-// import shoppingCart from './modules/shoppingCart'
+import user from './modules/user'
+import shoppingCart from './modules/shoppingCart'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: true,
   modules: {
-    // user,
-    // shoppingCart
+    user,
+    shoppingCart
   }
 })
